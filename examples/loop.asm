3b32bf6c3bba904025d75ee21074ddce9cce8015
Sam McGowan <the.sam.mcg@protonmail.com>
2023-02-06 20:01:37 +0000

    Fix loop example.

--- examples/loop.asm
+++ examples/loop.asm
@@ -1,19 +1,19 @@
 #include "../asm/lib.asm"
 
-movi 48, r0
-movi 58, r1
-
-movi 1, io_addr
-
 start:
-loop:
-    mov r0, io_data
-    io_out
+    movi 48, r0
+    movi 58, r1
 
-    movi 10, io_data
-    io_out
+    movi 1, io_addr
 
-    incr 1, r0
-    bri_ltu r0 r1, loop
+    loop:
+        mov r0, io_data
+        io_out
 
-halt
+        movi 10, io_data
+        io_out
+
+        incr 1, r0
+        bri_ltu r0 r1, loop
+
+    halt
